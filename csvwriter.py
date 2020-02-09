1371280c442b1f7f6ffabeb1b6b211c480f91a66
gao5254 <gao5254@qq.com>
2020-02-09 11:58:23 +0800

    完善csvwriter

--- csvwriter.py
+++ csvwriter.py
@@ -1,6 +1,6 @@
-import sys, csv
+import csv
 from PyQt5.QtCore import QObject, QDateTime
-from PyQt5.QtWidgets import QFileDialog, QApplication, QWidget
+from PyQt5.QtWidgets import QFileDialog, QWidget
 
 class csvWriter(QObject):
     def __init__(self, parent):
@@ -17,6 +17,7 @@ class csvWriter(QObject):
         filename = QFileDialog.getSaveFileName(self.parentWidget, "choose a path to save file", 
         QDateTime.currentDateTime().toString("yyyyMMdd-HHmm") + ".csv", "csv files(*.csv)")
         if filename[0] == "":
+            self.csvfile = None
             return False
         # 打开文件
         self.csvfile = open(filename[0], mode = "w", newline = "")
@@ -29,6 +30,8 @@ class csvWriter(QObject):
 
         供主程序调用，返回写入是否成功
         '''
+        if self.csvfile == None:
+            return False
         writer = csv.writer(self.csvfile)
         writer.writerow([curTime] + distances + [curAngle])
         return True
@@ -38,21 +41,30 @@ class csvWriter(QObject):
 
         供主程序调用，返回关闭是否成功
         '''
+        if self.csvfile == None:
+            return False
         self.csvfile.close()
         self.csvfile = None
         return True
 
 
 if __name__ == "__main__":
+    import sys, time
+    from PyQt5.QtWidgets import QApplication
+
     app = QApplication(sys.argv)
     window = QWidget()
     window.show()
     dataWriter = csvWriter(window)
     # ----
     dataWriter.start_writing()
-    dataWriter.write_distances(1,[3,2,1], 1.0)
-    dataWriter.write_distances(2,[1,2,3], 1.0)
-    dataWriter.write_distances(2,[1,2,3], 1.0)
+    thistime = time.time()
+    for i in range(100):
+        dataWriter.write_distances(1,[3,2,1], 1.0)
+        dataWriter.write_distances(2,[1,2,3], 1.0)
+        dataWriter.write_distances(2,[1,2,3], 1.0)
+    thattime = time.time()
     dataWriter.stop_writing()
+    print(thattime - thistime)
     # ----
     sys.exit(app.exec_())
