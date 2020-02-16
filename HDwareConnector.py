524fadb7f082fc702180f7b65e0ed4723771485d
gao5254 <gao5254@qq.com>
2020-02-16 19:57:41 +0800

    根据主程序，调整内部逻辑

--- HDwareConnector.py
+++ HDwareConnector.py
@@ -1,13 +1,12 @@
-import pyMT3 as mt
+# import pyMT3 as mt
+import fakeMT as mt
 import time
-from PyQt5.QtCore import QThread, pyqtSignal, QMutex
+from PyQt5.QtCore import QThread, pyqtSignal, QMutex, QObject
 
 class MyThread(QThread):
 
-    # 设备打开成功信号
-    openFinished = pyqtSignal(int)
     # 发生错误，错误代码
-    errorOccured = pyqtSignal(int)
+    errorHappened = pyqtSignal(int)
 
     def __init__(self):
         super().__init__()
@@ -19,7 +18,7 @@ class MyThread(QThread):
         # 定义扫描开关
         self.isScanning = False
         # 起始,读取时间
-        self.beginTime = time.time()
+        # self.beginTime = time.time()
         self.readingTime = time.time()
         # 通信句柄，index
         self.handle = 0.0
@@ -28,28 +27,30 @@ class MyThread(QThread):
     def run(self):
         while self.isScanning:
             (err, num, _) = mt.get_number(self.handle, self.ind)
-            self.threadLock.lock()
             if err == 0:
+                self.threadLock.lock()
                 self.distance = num
+                self.readingTime = time.time()
+                self.threadLock.unlock()
             else:
-                self.errorOccured.emit(err)
-            self.threadLock.unlock()
+                self.errorHappened.emit(err)
+                break
 
     def my_open_device(self, portNum, openedNum):
         """
         打开设备
         :param portNum: 串口号（int）
         :param openedNum: 设备序号（int）
-        :return: 是否打开（bool（0：成功；1：失败））
+        :return: 是否打开（bool（1：成功；0：失败））
         """
         (err, self.handle, self.ind) = mt.open_device(portNum)
         if err == 0:
-            self.openFinished.emit(openedNum)
-            self.beginTime = time.time()
-            return False
-        else:
-            self.errorOccured.emit(err)
+            # self.openFinished.emit(openedNum)
+            # self.beginTime = time.time()
             return True
+        else:
+            self.errorHappened.emit(err)
+            return False
 
     def my_start(self):
         """
@@ -66,14 +67,16 @@ class MyThread(QThread):
         :return: 激光位移传感器读数和时间
         """
         self.threadLock.lock()
-        self.readingTime = time.time()
-        return self.distance, self.readingTime - self.beginTime
-
-    def my_unlock(self):
-        """
-        关闭线程锁 
-        """
+        num = self.distance
+        readTime = self.readingTime
         self.threadLock.unlock()
+        return (num, readTime)
+
+    # def my_unlock(self):
+    #     """
+    #     关闭线程锁 
+    #     """
+    #     self.threadLock.unlock()
 
     def my_end(self):
         """
@@ -83,12 +86,24 @@ class MyThread(QThread):
         self.isScanning = False
         mt.close_device(self.handle)
 
-class hdwareConnector():
+class hdwareConnector(QObject):
+
+    # 设备打开成功信号
+    openFinished = pyqtSignal(int)
+    # 发生错误，错误代码
+    errorOccured = pyqtSignal(int)
+
+
     def __init__(self):
+        super(hdwareConnector, self).__init__()
         # 定义三个线程对象
-        self.thread1 = MyThread()
-        self.thread2 = MyThread()
-        self.thread3 = MyThread()
+        self.threads = [MyThread(), MyThread(), MyThread()]
+        # 信号连接
+        for i in range(3):
+            self.threads[i].errorHappened.connect(self.OnErrorHappened)
+
+    def OnErrorHappened(self, err):
+        self.errorOccured.emit(err)
 
     def open_devices(self, portNum):
         """
@@ -96,21 +111,32 @@ class hdwareConnector():
         :param portNum: 串口号（list）
         :return: 是否打开成功（bool（0：失败；1：成功））
         """
-        # 开设备
-        if self.thread1.my_open_device(portNum[0], 1) or\
-            self.thread2.my_open_device(portNum[1], 2) or\
-                self.thread3.my_open_device(portNum[2], 3):
-            self.close_devices()
-            return False
+        # # 开设备
+        # if self.thread1.my_open_device(portNum[0], 1) or\
+        #     self.thread2.my_open_device(portNum[1], 2) or\
+        #         self.thread3.my_open_device(portNum[2], 3):
+        #     self.close_devices()
+        #     return False
 
-        # 开始扫描
-        if self.thread1.my_start() and\
-            self.thread2.my_start() and\
-                self.thread3.my_start():
-                return True
-        else:
-            self.close_devices()
-            return False
+        # # 开始扫描
+        # if self.thread1.my_start() and\
+        #     self.thread2.my_start() and\
+        #         self.thread3.my_start():
+        #         return True
+        # else:
+        #     self.close_devices()
+        #     return False
+        for i in range(3):
+            if not self.threads[i].my_open_device(portNum[i], i):
+                self.close_devices()
+                return False
+        for i in range(3):
+            if not self.threads[i].my_start():
+                self.close_devices()
+                return False
+            self.openFinished.emit(i+1)
+        return True
+            
 
     def get_distances(self):
         """
@@ -120,13 +146,10 @@ class hdwareConnector():
         distances = [0.0, 0.0, 0.0]
         readingTimes = [0.0, 0.0, 0.0]
 
-        [distances[0], readingTimes[0]] = self.thread1.my_read()
-        self.thread1.my_unlock()
-        [distances[1], readingTimes[1]] = self.thread2.my_read()
-        self.thread2.my_unlock()
-        [distances[2], readingTimes[2]] = self.thread3.my_read()
-        self.thread3.my_unlock()
+        for i in range(3):
+            (distances[i], readingTimes[i]) = self.threads[i].my_read()
 
+        # 取三个读取时间的均值作为实际时间
         readingTime = sum(readingTimes) / 3
         returnValue = (readingTime, distances)
 
@@ -137,18 +160,20 @@ class hdwareConnector():
         接口函数，关闭设备，关闭线程
         :return: 关闭成功（bool（0：失败；1：成功））
         """
-        self.thread1.my_end()
-        self.thread2.my_end()
-        self.thread3.my_end()
-        self.thread1.terminate()
-        self.thread2.terminate()
-        self.thread3.terminate()
-        if self.thread1.isFinished() and\
-            self.thread2.isFinished() and\
-                self.thread3.isFinished():
-            return True
-        else:
-            return False
+        for i in range(3):
+            self.threads[i].my_end()
+            self.threads[i].quit()
+
+        return True
 
 
+if __name__ == "__main__":
+    myConnector = hdwareConnector()
+    myConnector.open_devices([4,5,6])
+    thistime = time.time()
+    for i in range(100):
+        myConnector.get_distances()
+    thattime = time.time()
+    print(thattime - thistime)
+    myConnector.close_devices()
 
